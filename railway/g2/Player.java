377976b21c2ce3e20219adca2deb55f977446dae
jifeng <jifeng@jifengdeMacBook-Pro.local>
2018-10-29 09:31:19 -0400

    Choose the link thaat has the highest value, no stop threshold for now

--- railway/g2/Player.java
+++ railway/g2/Player.java
@@ -45,9 +45,13 @@ public class Player implements railway.sim.Player {
         // This checks whether we are in the same round.
         // Random player doesn't care about bids made by other players.
 
-        if (availableBids.size() != 0) {
-            return null;
-        }
+        // System.out.println("1 THIS SHOULD PRINT TWICE");
+
+        // if (availableBids.size() != 0) {
+        //     return null;
+        // }
+        // System.out.println("2 THIS SHOULD PRINT TWICE");
+
 
         for (BidInfo bi : allBids) {
             if (bi.owner == null) {
@@ -55,10 +59,12 @@ public class Player implements railway.sim.Player {
             }
         }
 
+
         if (availableBids.size() == 0) {
             return null;
         }
-
+        // bi.amount = transit[t1][t2] * li.distance * 10
+        //so sort availableBids by amount and choose the highest
         Collections.sort(availableBids,new Comparator<BidInfo>(){
             public int compare(BidInfo o1, BidInfo o2) {
                 if(o1.amount > o2.amount){
@@ -70,9 +76,9 @@ public class Player implements railway.sim.Player {
                 return -1;
             }
         });
-        for(int i=0;i<availableBids.size();i++){
-            availableBids.get(i).amount=availableBids.get(i).amount+100;
-        }
+        // for(int i=0;i<availableBids.size();i++){
+        //     availableBids.get(i).amount=availableBids.get(i).amount+100;
+        // }
         BidInfo curBid = availableBids.get(availableBids.size()-1);
         double amount = curBid.amount;
 
@@ -83,14 +89,24 @@ public class Player implements railway.sim.Player {
             return null;
         }
 
+        System.out.print("Current bid: " + currentBids.size());
+
         // Check if another player has made a bid for this link.
         for (Bid b : currentBids) {
+            // System.out.println("amount+bidder"+b.amount+b.bidder);
             if (b.id1 == curBid.id || b.id2 == curBid.id) {
-                if (budget - b.amount - 1 < 0.) {
+                if (budget - b.amount - 10000 < 0.) {
                     return null;
                 }
                 else {
-                    amount = b.amount + 1;
+                    if(b.bidder.equals("g2"))
+                    {
+                        break;
+                    }
+                    // System.out.println(availableBids.size() + " " + allBids.size());
+
+                    amount = b.amount + 10000;
+                    // System.out.println("Our latest bid: " + amount);
                 }
 
                 break;
@@ -100,7 +116,7 @@ public class Player implements railway.sim.Player {
         Bid bid = new Bid();
         bid.amount = amount;
         bid.id1 = curBid.id;
-
+        // availableBids = new ArrayList<>();
         return bid;
     }
 
