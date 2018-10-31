0d0bbe261418e53799129d159c5356975bef9d90
Sahil Shah <sahilshah@Sahils-MacBook-Pro.local>
2018-10-31 05:57:22 -0400

    Player checks most valuable node, updates traffic, bids till expected value

--- railway/g2/Player.java
+++ railway/g2/Player.java
@@ -20,6 +20,7 @@ class AvailableLinks {
     public String town2;
     public int town2_id;
     public double traffic;
+    public double expectedTraffic;
     public double distance;
     public double originalValue;
     public double bidValue;
@@ -84,25 +85,48 @@ public class Player implements railway.sim.Player {
 
         //get the weightedGraph given node s
         weightedGraph=initializeWeightedGraph(s,townLookup,infra);
-        weightedGraph.print();
+        //weightedGraph.print();
         //implement dijkstra to the weightedGraph
         int[][] prediction = Dijkstra.dijkstra(weightedGraph, s);
+
+        //printAvailableLinks();
+
         for (int n=0;n<townLookup.size();n++){
-            System.out.println(weightedGraph.getLabel(s) + " to " + weightedGraph.getLabel(n));
-            Dijkstra.printPath(weightedGraph, prediction, n);
+            //System.out.println(weightedGraph.getLabel(s) + " to " + weightedGraph.getLabel(n));
+            //Dijkstra.printPath(weightedGraph, prediction, n);
+
+            List<List<Integer>> allPaths = Dijkstra.getPaths(weightedGraph, prediction, n);
+            int number_of_paths = allPaths.size();
+            for(List<Integer> a: allPaths){
+            	if(a.size() < 2)
+            		continue;
+
+            	int t1 = a.get(0);
+            	int t2 = a.get(1);
+            	int total_traffic = transit[t1][n] + transit[n][t1];
+            	updateExpectedTraffic(t1, t2, total_traffic);
+            	//System.out.println("Add to from " + t1 + " to " + t2);
+            	//System.out.println("Traffic 1: " + transit[t1][n] + " Traffic 2: " + transit[n][t1]);
+
+            	//find t1 - t2 or t2 - t1 link and add ())
+
+            }
             //System.out.println();
         }
 
+        //printAvailableLinks();
+
+
         //System.out.println(name + " has budget: " + budget);
         
-        for(int i=0;i<infra.size();i++){
-            for(int j=0;j<infra.get(i).size();j++){
-                //System.out.println(" infra shows as :"+i+" to "+infra.get(i).get(j));
-            }
-        }
+        //for(int i=0;i<infra.size();i++){
+        //    for(int j=0;j<infra.get(i).size();j++){
+        //        //System.out.println(" infra shows as :"+i+" to "+infra.get(i).get(j));
+        //    }
+        //}
         
 
-        for (BidInfo bi : allBids) {
+        /*for (BidInfo bi : allBids) {
             //System.out.println("Bid id: " + bi.id + " Bid from " + bi.town1 + " to " + bi.town2 + " made by " + bi.owner + " for: " + bi.amount);
             if (bi.owner == null) {
                 bi.amount += 1111;
@@ -112,9 +136,25 @@ public class Player implements railway.sim.Player {
 
         if (availableBids.size() == 0) {
             return null;
-        }
+        }*/
 
-        Collections.sort(availableBids,new Comparator<BidInfo>(){
+        if(availableLinks.size() == 0)
+        	return null;
+
+        //printAvailableLinks();
+        Collections.sort(availableLinks, new Comparator<AvailableLinks>(){
+        	public int compare(AvailableLinks l1, AvailableLinks l2){
+        		if(l1.expectedValue > l2.expectedValue)
+        			return 1;
+        		if(l1.expectedValue == l2.expectedValue)
+        			return 0;
+        		return -1;
+        	}
+        });
+        //System.out.println("------------------- AFTER SORTING ---------------------");
+        //printAvailableLinks();
+
+        /*Collections.sort(availableBids,new Comparator<BidInfo>(){
             public int compare(BidInfo o1, BidInfo o2) {
                 if(o1.amount > o2.amount){
                     return 1;
@@ -124,10 +164,19 @@ public class Player implements railway.sim.Player {
                 }
                 return -1;
             }
-        });
+        });*/
 
-        BidInfo randomBid = availableBids.get(availableBids.size()-1);
-        double amount = randomBid.amount;
+        AvailableLinks link_to_bid = availableLinks.get(availableLinks.size()-1);
+        BidInfo ourBid = new BidInfo();
+        ourBid.id = link_to_bid.id;
+        ourBid.town1 = link_to_bid.town1;
+        ourBid.town2 = link_to_bid.town2;
+        ourBid.amount = link_to_bid.originalValue;
+        ourBid.owner = null;
+
+
+        //BidInfo randomBid = availableBids.get(availableBids.size()-1);
+        double amount = ourBid.amount;
 
         // Don't bid if the random bid turns out to be beyond our budget.
         if (budget - amount < 0.) {
@@ -136,29 +185,29 @@ public class Player implements railway.sim.Player {
 
         // Check if another player has made a bid for this link.
         for (Bid b : currentBids) {
-            if (b.id1 == randomBid.id || b.id2 == randomBid.id) {
+            if (b.id1 == ourBid.id || b.id2 == ourBid.id) {
                 if (budget - b.amount - 10000 < 0.) {
                     return null;
                 }
-                else if (amount>randomBid.amount+30000){
-                    //System.out.println("Stop bidding");
-                    break;
-                }
-                else {
-                    if(b.bidder.equals("g2"))
-                    {
-                        return null;
-                    }
-                    amount = b.amount + 10000;
+                if(b.bidder.equals("g2")){
+                    return null;
                 }
 
+                amount = b.amount + 10000;
+                if(amount > link_to_bid.expectedValue)
+                	return null;
                 break;
             }
         }
 
+        //System.out.println("Link from " + link_to_bid.town1 + " to " + link_to_bid.town2);
+        //System.out.println("Original traffic: " + link_to_bid.traffic + " Expected traffic: " + link_to_bid.expectedTraffic);
+        //System.out.println("Original val: " + link_to_bid.originalValue + " Expected Val: " + link_to_bid.expectedValue);
+        //System.out.println();
+
         Bid bid = new Bid();
         bid.amount = amount;
-        bid.id1 = randomBid.id;
+        bid.id1 = ourBid.id;
 
         return bid;
     }
@@ -191,7 +240,10 @@ public class Player implements railway.sim.Player {
 
                 link.distance = getDistance(link.town1_id, link.town2_id);
                 link.traffic = transit[link.town1_id][link.town2_id];
-                link.originalValue = bid.amount;
+                link.originalValue = link.distance*link.traffic*10;
+                link.expectedTraffic = link.traffic;	//reset expected traffic
+                link.expectedValue = link.originalValue;
+
                 //System.out.println(link.id + " " + link.town1 + " " + link.town2 + " " + link.originalValue + " " + link.bidValue + " " + 
                 //    link.distance + " " + link.traffic);
                 //System.out.println("Expected Value: " + (link.distance*link.traffic*10));
@@ -271,10 +323,33 @@ public class Player implements railway.sim.Player {
     	return mvn;
     }
 
+    private void updateExpectedTraffic(int town1_id, int town2_id, double addedTraffic){
+    	for(int i=0; i<availableLinks.size(); i++){
+    		AvailableLinks currentLink = availableLinks.get(i);
+    		int t1_id = currentLink.town1_id;
+    		int t2_id = currentLink.town2_id;
+
+    		if((t1_id==town1_id && t2_id==town2_id) || (t1_id==town2_id && t2_id==town1_id)){
+    			currentLink.expectedTraffic += addedTraffic;
+    			currentLink.expectedValue = currentLink.expectedTraffic*currentLink.distance*10;
+    		}
+
+    	}
+    }
+
     private double getDistance(int t1, int t2) {
         return Math.pow(
             Math.pow(geo.get(t1).x - geo.get(t2).x, 2) +
                 Math.pow(geo.get(t1).y - geo.get(t2).y, 2),
             0.5);
+    }
+
+    private void printAvailableLinks(){
+    	for(int i=0; i<availableLinks.size(); i++){
+    		AvailableLinks currentLink = availableLinks.get(i);
+    		System.out.println("From " + currentLink.town1_id + currentLink.town1 + " to " + currentLink.town2_id + currentLink.town2 +
+    			" original traffic: " + currentLink.traffic + " expected traffic: " + currentLink.expectedTraffic + 
+    			" original value: " + currentLink.originalValue + " expected value: " + currentLink.expectedValue);
+    	}
     }
 }
